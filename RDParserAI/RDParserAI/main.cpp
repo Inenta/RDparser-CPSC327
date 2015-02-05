02240f1754dd5aff41b559e76cd239662b206fc3
Inenta <inenta117@gmail.com>
2015-02-04 18:39:17 -0800

    All the code + txt file, seg fault
    OK FIX THE THING LOL

--- RDParserAI/RDParserAI/main.cpp
+++ RDParserAI/RDParserAI/main.cpp
@@ -1,8 +1,9 @@
 //
 //  main.cpp
 //  RDParser
-//
-//  Created by Matthew Voegeli on 2/2/15.
+//  This recursive descent parser is being used to evaluate logical terms to see if
+//  a logical sentence is either true or false.
+//  Created by Matthew Voegeli, Trevor Berreth, and Lucas Orlita on 2/2/15.
 //  Copyright (c) 2015 Matthew Voegeli. All rights reserved.
 //
 
@@ -10,7 +11,7 @@
 #include <cstdio>
 #include <vector>
 #include <cctype>
-#include <String>
+#include <string>
 #include <fstream>
 using namespace std;
 
@@ -33,7 +34,7 @@ struct Token
     TOKENTYPE type;
     char val;
 };
-
+//Prototypes
 void readFromFile();
 void Eat(Token * tokens, int& index);
 TOKENTYPE Peek(Token * tokens, int index);
@@ -44,10 +45,10 @@ bool AndExpression(Token * tokens, int& index);
 bool NegateExpression(Token * tokens, int& index);
 bool Expression(Token * tokens, int& index);
 bool LiteralExpression(Token * tokens, int& index);
+void Tokenizer(Token * tokens, string line);
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    readFromFile();
 }
 
 void readFromFile()
@@ -58,8 +59,13 @@ void readFromFile()
     {
         while(getline(myfile, line))
         {
-            cout << line << '\n';
-            //function call to evaluate it
+            cout << "Getting line" << endl;
+            int index = 0;
+            Token tokens [50];
+            cout << line << endl;
+            
+            Tokenizer(tokens, line);
+            cout << BiconditionalExpression(tokens, index) << endl;
         }
     }
     
@@ -134,7 +140,7 @@ bool Expression(Token * tokens, int& index){
     else
         return LiteralExpression(tokens, index);
 }
-//This function is used as a grammer describes that of the 'true' or 'false which is as a true or false statement.
+//This function is used as a grammer describes that of the 'true' or 'false' which is as a true or false statement.
 bool LiteralExpression(Token * tokens, int& index) {
     if(Peek(tokens, index) == BOOLTRUE){
         Eat(tokens, index);
@@ -144,6 +150,85 @@ bool LiteralExpression(Token * tokens, int& index) {
         return false;
     }
 }
+    void Tokenizer(Token * tokens, string line){
+       
+        //Used to take the string from a line of the file and make it an array of characters
+        int TempNumOne = line.size();
+        char logicalSentence[50];
+        for (int i = 0; i <= TempNumOne; i++)
+        {
+            logicalSentence [i] = line[i];
+        }
+        int tokensIndex = 0;
+        cout << "In Token my nigga" << endl;
+        for (int j = 0; j <= TempNumOne; j++)
+        {
+            if(logicalSentence[j] == 't')
+            {
+                Token * token;
+                token->type = BOOLTRUE;
+                tokens[tokensIndex] = *token;
+                tokensIndex++;
+                delete token;
+                j += 3;
+                break;
+            } else if(logicalSentence[j] == 'f') {
+                Token * token;
+                token->type = BOOLFALSE;
+                tokens[tokensIndex] = *token;
+                tokensIndex++;
+                delete token;
+                j += 4;
+            } else if(logicalSentence[j] == '~') {
+                Token * token;
+                token->type = NEGATE;
+                tokens[tokensIndex] = *token;
+                tokensIndex++;
+                delete token;
+            } else if(logicalSentence[j] == '&') {
+                Token * token;
+                token->type = AND;
+                tokens[tokensIndex] = *token;
+                tokensIndex++;
+                delete token;
+            } else if(logicalSentence[j] == '|') {
+                Token * token;
+                token->type = OR;
+                tokens[tokensIndex] = *token;
+                tokensIndex++;
+                delete token;
+            } else if(logicalSentence[j] == '=' && logicalSentence[j+1] == '>') {
+                Token * token;
+                token->type = IMPLICATION;
+                tokens[tokensIndex] = *token;
+                tokensIndex++;
+                delete token;
+                j ++;
+            } else if(logicalSentence[j] == '<' && logicalSentence[j+1] == '=' && logicalSentence[j+2] == '>') {
+                Token * token;
+                token->type = BICONDITIONAL;
+                tokens[tokensIndex] = *token;
+                tokensIndex++;
+                delete token;
+                j += 2;
+            } else if(logicalSentence[j] == '(') {
+                Token * token;
+                token->type = LEFTPAREN;
+                tokens[tokensIndex] = *token;
+                tokensIndex++;
+                delete token;
+            } else if(logicalSentence[j] == ')') {
+                Token * token;
+                token->type = RIGHTPAREN;
+                tokens[tokensIndex] = *token;
+                tokensIndex++;
+                delete token;
+            } else
+                break;
+            cout << "Finished part of FORLOOP" << endl;
+        }
+    }
+
 
 
 
